1cd0848b146ecf28f0bf949d949c4114edb1e752
Delta-do <pumponisharf@mail.ru>
2022-10-12 22:08:23 +0300

    Added task 3

--- c_lab6/Source.c
+++ c_lab6/Source.c
@@ -18,27 +18,48 @@ void t2()
 	double x;
 	printf("x = ");
 	scanf_s("%lf", &x);
-	/*
-	if (x > 3)
-		printf("%f", -3. * x + 9);
-	else
-		printf("%f", pow(x, 3) / (pow(x, 2) + 8.));
-		*/
 	printf("F(%.3f) = %.3f", x, x > 3 ? ( - 3. * x + 9) : (double) pow(x, 3) / (pow(x, 2) + 8));
 }
 
 void t3()
 {
-	char* name1 = gets();
-	char name2[10];
+	char name1[20];
+	char name2[20];
 	int d1, d2, m1, m2, g1, g2;
-	puts("Введите имя и дату рождения 1 человека");
-	scanf_s("%d %d %d", &d1, &m1, &g1);
-	//puts("Введите имя и дату рождения 2 человека");
-	//scanf_s("%[] %d %d %d", &name2, &d2, &m2, &g2);
 
-	printf("%s %d %d %d", name1, d1, m1, g1);
-	//printf("%s %d %d %d", name2, d2, m2, g2);
+	puts("Введите имя 1 человека");
+	gets(name1);
+
+	puts("Введите имя 2 человека");
+	gets(name2); 
+
+	puts("Введите дату рождения 1 человека через пробел");
+	scanf_s("%d %d %d", &d1, &m1, &g1);
+
+	//puts("Введите имя 2 человека");
+	//gets(name2); //???????????????
+	puts("Введите дату рождения 2 человека через пробел");
+	scanf_s("%d %d %d", &d2, &m2, &g2);
+	
+	//printf("%s %d %d %d\n", name1, d1, m1, g1);
+	//printf("%s %d %d %d\n", name2, d2, m2, g2);
+
+	if (g1 > g2)
+		printf("%s", name2);
+	else
+		if (g1 < g2)
+			printf("%s", name1);
+		else
+			if (m1 > m2)
+				printf("%s", name2);
+			else
+				if (m1 < m2)
+					printf("%s", name1);
+				else
+					if (d1 > d2)
+						printf("%s", name2);
+					else
+						printf("%s", name1);
 }
 
 void main()
